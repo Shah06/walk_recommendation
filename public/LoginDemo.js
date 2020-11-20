9bae1a51b3b3876f14e9b57ba2be3cd5b60c7684
fpeng-02 <66930463+fpeng-02@users.noreply.github.com>
2020-11-19 20:10:44 -0800

    Add files via upload
    Rough login page. JS sends login info to the server and stores the resulting user id in a cookie. Does not account for bad logins.

---
+++ public/LoginDemo.js
@@ -0,0 +1,16 @@
+axios.defaults.headers.common['X-Auth-Token'] =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+function login() {
+    axios.post('http://localhost/login', {
+        name: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    })
+    .then ((response) => {
+    	console.log(response);
+        Cookies.set(document.getElementById('username').value, response, { expires: 7 });
+    }, (error) => {
+        console.log(error);
+    })
+} 
+
+document.getElementById('submit').addEventListener('click', login); 
