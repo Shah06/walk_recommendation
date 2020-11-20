01dd01619fa973a22cc5ab5f8336331719cddbf8
fpeng-02 <66930463+fpeng-02@users.noreply.github.com>
2020-11-19 20:18:51 -0800

    Update LoginDemo.js

--- public/LoginDemo.js
+++ public/LoginDemo.js
@@ -2,12 +2,12 @@ axios.defaults.headers.common['X-Auth-Token'] =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 function login() {
     axios.post('http://localhost/login', {
-        name: document.getElementById('username').value,
+        name: document.getElementById('email').value,
         password: document.getElementById('password').value
     })
     .then ((response) => {
     	console.log(response);
-        Cookies.set(document.getElementById('username').value, response, { expires: 7 });
+        Cookies.set(document.getElementById('email').value, response, { expires: 7 });
     }, (error) => {
         console.log(error);
     })
