0de7ce0df47063e6dbc2a429440a7ba1595a6f83
fpeng-02 <fpeng2002@gmail.com>
2020-11-21 19:11:56 -0800

    Update LoginDemo.js

--- public/LoginDemo.js
+++ public/LoginDemo.js
@@ -7,7 +7,7 @@ function login() {
     .then ((response) => {
         const resp = response.data;
     	console.log(resp);
-        Cookies.set(document.getElementById('email').value, resp.slice(1, resp.length-1), { expires: 7 });
+        Cookies.set("sessionID", resp.slice(1, resp.length-1), { expires: 7 });
     }, (error) => {
         document.getElementById("loginmessage").innerHTML+="Login failed. Username or password is wrong.";
         console.log("hey :)");
