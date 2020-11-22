fa02c6866a82827e840717c0bcc6c3710e79fee8
fpeng-02 <fpeng2002@gmail.com>
2020-11-21 21:47:52 -0800

    Implemented Weather Display in Home.html
    Home.html now displays weather for a user using the sessionID stored in cookies.

--- public/Home.js
+++ public/Home.js
@@ -1,9 +1,22 @@
-axios.post('http://localhost:80/weather', {
-    sessionId: Cookies.get("SessionID"),
-})
-.then ((response) => {
-    const resp = response.data;
-    console.log(resp);
-}, (error) => {
-    console.log(error);
-})
+function UpdateWeather() {
+    axios.post('http://localhost:80/weather', {
+
+        sessionId: Cookies.get("sessionID")
+    })
+    .then ((response) => {
+        var resp = response.data.list;
+        console.log(response);
+        var i;
+        for (i = 0; i<5; i++) {
+            var newWeatherText = "The weather at " + (resp[i]["dt_txt"]) +" is " + (Math.trunc(resp[i]['main']['temp']-273.15)) + " degrees Celsius";
+            var newWeatherPh = document.createElement("p").appendChild(document.createTextNode(newWeatherText));
+            document.getElementById("weatherlist").appendChild(newWeatherPh);
+            document.getElementById("weatherlist").appendChild(document.createElement("br"));
+        }
+        
+    }, (error) => {
+        console.log(Cookies.get("sessionID"));
+        console.log(error);
+    })
+}
+UpdateWeather();
