c59d64aa53251ec03285882c401414f2d4c832d6
fpeng-02 <fpeng2002@gmail.com>
2020-11-28 18:50:26 -0800

    Implementing weather recommend

--- public/Home.js
+++ public/Home.js
@@ -1,22 +1,42 @@
-function UpdateWeather() {
+var weatherArray= [];
+function updateWeather() {
     axios.post('http://localhost:80/weather', {
-
         sessionId: Cookies.get("sessionID")
     })
     .then ((response) => {
         var resp = response.data.list;
         console.log(response);
         var i;
+        weatherArray = [];
         for (i = 0; i<5; i++) {
-            var newWeatherText = "The weather at " + (resp[i]["dt_txt"]) +" is " + (Math.trunc(resp[i]['main']['temp']-273.15)) + " degrees Celsius";
+            var currentTemp = (resp[i]['main']['temp']-273.15).toPrecision(3);
+            var currentTime = new Date(resp[i]["dt"]*1000);
+            console.log(currentTime);
+            weatherArray.push([currentTime, currentTemp]);
+            var newWeatherText = "The weather at " + (currentTime) +" is " + (currentTemp) + " degrees Celsius";
             var newWeatherPh = document.createElement("p").appendChild(document.createTextNode(newWeatherText));
-            document.getElementById("weatherlist").appendChild(newWeatherPh);
-            document.getElementById("weatherlist").appendChild(document.createElement("br"));
+            //Append the weather data to the docuemnt
+            document.getElementById("weatherlisting").appendChild(newWeatherPh);
+            //Create a line break
+            document.getElementById("weatherlisting").appendChild(document.createElement("br"));
         }
-        
     }, (error) => {
         console.log(Cookies.get("sessionID"));
         console.log(error);
     })
 }
-UpdateWeather();
+function recommendWeather() {
+    var i;
+    for (i = 0; i<weatherArray.length ; i++)
+    {
+        var currentElement = weatherArray[i];
+        if (currentElement[1]>7 && currentElement[1]<26)
+        {
+            var recText = weatherArray[i][0].getHours() + " is a good time to take a walk";
+            document.getElementById("walkRecommendation").appendChild(recText);
+            document.getElementById("walkRecommendation").appendChild(document.createElement("br"));
+        }
+    }
+}
+updateWeather();
+recommendWeather();
