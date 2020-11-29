293db590f3c9051f286d87e37b7de48ace2e47a1
fpeng-02 <fpeng2002@gmail.com>
2020-11-28 19:41:30 -0800

    Homepage now suggests map/gives a map of your location
    Map currently asks for user location.

--- public/Home.js
+++ public/Home.js
@@ -1,4 +1,6 @@
 var weatherArray= [];
+/** The function gets weather data for the next 15 hours and stores it in an array
+*/
 function updateWeather() {
     axios.post('http://localhost:80/weather', {
         sessionId: Cookies.get("sessionID")
@@ -11,15 +13,13 @@ function updateWeather() {
         for (i = 0; i<5; i++) {
             var currentTemp = (resp[i]['main']['temp']-273.15).toPrecision(3);
             var currentTime = new Date(resp[i]["dt"]*1000);
-            console.log(currentTime);
             weatherArray.push([currentTime, currentTemp]);
             var newWeatherText = "The weather at " + (currentTime) +" is " + (currentTemp) + " degrees Celsius";
             var newWeatherPh = document.createElement("p").appendChild(document.createTextNode(newWeatherText));
-            //Append the weather data to the docuemnt
             document.getElementById("weatherlisting").appendChild(newWeatherPh);
-            //Create a line break
             document.getElementById("weatherlisting").appendChild(document.createElement("br"));
         }
+        recommendWeather();
     }, (error) => {
         console.log(Cookies.get("sessionID"));
         console.log(error);
@@ -27,17 +27,25 @@ function updateWeather() {
 }
 function recommendWeather() {
     var i;
+    console.log(weatherArray.length);
+    var isWalk = false;
+    var minTemp = 15.55;
+    var maxTemp = 26;
     for (i = 0; i<weatherArray.length ; i++)
     {
-        console.log("hhee ehehe");
         var currentElement = weatherArray[i];
-        if (currentElement[1]>7 && currentElement[1]<26)
+        if (currentElement[1] > minTemp && currentElement[1] < maxTemp)
         {
             var recText = weatherArray[i][0].getHours() + " is a good time to take a walk";
-            document.getElementById("walkRecommendation").appendChild(recText);
+            var recPh = document.createElement("p").appendChild(document.createTextNode(recText));
+            document.getElementById("walkRecommendation").appendChild(recPh);
             document.getElementById("walkRecommendation").appendChild(document.createElement("br"));
         }
     }
+    if(!isWalk)
+    {
+        var recNoPh = document.createElement("p").appendChild(document.createTextNode("There aren't any good times to walk. Check back later."));
+        document.getElementById("walkRecommendation").appendChild(recNoPh);
+    }
 }
 updateWeather();
-recommendWeather();
