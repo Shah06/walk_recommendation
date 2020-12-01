9d8e1358c141b4a7cb4661d27b4ee1c05bf10020
fpeng-02 <fpeng2002@gmail.com>
2020-11-30 23:39:01 -0800

    Update Home.js
    improved weather algorithm/cleaned up home.js

--- public/Home.js
+++ public/Home.js
@@ -16,12 +16,16 @@ function updateWeather() {
             var currentTime = new Date(resp[i]["dt"]*1000);
             var weatherCondition = resp[i]['weather'][0]['id'];
             weatherArr.push([currentTime, currentTemp, weatherCondition]);
+
+            //Add the text to the html element. Change the text/location here
             var newWeatherText = "The weather at " + (currentTime) +" is " + (currentTemp) + " degrees Celsius";
             var newWeatherPh = document.createElement("p").appendChild(document.createTextNode(newWeatherText));
             document.getElementById("weatherlisting").appendChild(newWeatherPh);
             document.getElementById("weatherlisting").appendChild(document.createElement("br"));
         }
+
         recommendWeather(weatherArr);
+
     }, (error) => {
         console.log(Cookies.get("sessionID"));
         console.log(error);
@@ -34,21 +38,51 @@ function recommendWeather(weatherArray) {
     var isWalk = false;
     var minTemp = 15;
     var maxTemp = 30;
-    var bestTemp;
+    var bestTemp = 100;
+    var bestIndex;
     for (i = 0; i<weatherArray.length ; i++)
     {
         var currentElement = weatherArray[i];
-        if (currentElement[1] > minTemp && currentElement[1] < maxTemp)
+        //Checks for time and makes sure there are no adverse weather conditions
+        if ((currentElement[1] >= minTemp && currentElement[1] <= maxTemp) && (currentElement[2]<=804 && currentElement[2]>=800))
         {
-            var recText = weatherArray[i][0].getHours() + "o'clock is a good time to take a walk";
+            //Change the text/location for good time to walk here
+            var recText = currentElement[0].getHours() + "o'clock is a good time to take a walk";
+            var recPh = document.createElement("p").appendChild(document.createTextNode(recText));
+            document.getElementById("walkRecommendation").appendChild(recPh);
+            document.getElementById("walkRecommendation").appendChild(document.createElement("br"));
+            isWalk = true;
+        }
+        if (Math.abs(currentElement[1]-minTemp)<bestTemp)
+        {
+            bestIndex = i;
+            bestTemp = currentElement[1];
+        }
+        if (Math.abs(currentElement[1]-maxTemp)<bestTemp)
+        {
+            bestIndex = i;
+            bestTemp = currentElement[1];
+        }
+    }
+    if(!isWalk)
+    {
+        if (bestTemp<minTemp)
+        {
+            console.log("No great times to walk.");
+            var recText = "Its a bit chilly but " + weatherArray[bestIndex][0].getHours() + "o'clock is the best time to walk";
+            var recPh = document.createElement("p").appendChild(document.createTextNode(recText));
+            document.getElementById("walkRecommendation").appendChild(recPh);
+            document.getElementById("walkRecommendation").appendChild(document.createElement("br"));
+
+        }
+        if(bestTemp>maxTemp)
+        {
+            console.log("No great times to walk.");
+            var recText = "Its a bit hot but " + weatherArray[bestIndex][0].getHours() + "o'clock is the best time to walk";
             var recPh = document.createElement("p").appendChild(document.createTextNode(recText));
             document.getElementById("walkRecommendation").appendChild(recPh);
             document.getElementById("walkRecommendation").appendChild(document.createElement("br"));
         }
-        Math.abs(currentElement[1]-15)
-    }
-    if(!isWalk)
-    {
 
         //var recNoPh = document.createElement("p").appendChild(document.createTextNode("There aren't any good times to walk. Check back later."));
         //document.getElementById("walkRecommendation").appendChild(recNoPh);
