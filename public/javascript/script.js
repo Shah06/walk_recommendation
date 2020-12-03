03e262da96860b1104610e5e384bc4851353ab85
ButterSticks3 <73627698+ButterSticks3@users.noreply.github.com>
2020-12-02 21:46:11 -0800

    renamed many files

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -16,6 +16,23 @@ export function passwordCheck() {
 }
 
 export function redirectToHome() {
-    const url = './Home.html';
+    const url = './index.html';
     window.location.assign(url);
+}
+export function logOut() {
+    const url ='./login.html';
+    deleteAllCookies();
+    window.location.assign(url);
+    document.getElementById('loginmessage').innerHTML = "You have successfully logged out";
+}
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
 }
